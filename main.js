73057133de2da717de812b7b6d181c90ba2857d6
Lê Ngoại Ngữ <lengoaingu@gmail.com>
2023-01-11 14:13:48 +0700

    Các hàm built in trong javascript

--- main.js
+++ main.js
@@ -1,6 +1,26 @@
-// Learn aobut variable
+/**
+ * 1. Alert
+ * 2. Console
+ * 3. Confirm
+ * 4. Prompt
+ * 5. Set timout
+ * 6. Set interval
+ */
 
-var fullName = "Hello F8";
+// alert("Hello world");
+console.log("12321lfjdklfjdls");
+console.error("This is error");
+console.warn("This is warning");
+console.info("This is info");
+// console.clear(); // sẽ clear toàn bộ log bên trên kể từ dòng code này
+// console.group("start"); // gom nhóm lại. cái này thì ít dùng
+//confirm("Bạn đã dủ 18 tuổi chưa?"); // trả về true hoặc false. Nếu trong trường hợp người dùng bấm ok => trả về true ngược lại trả về false
+// const value = prompt("Nhập tuổi của bạn vào đây"); // sẽ trả về string mà người dùng nhập vào hoặc null trong trường hợp không nhập gì
+// console.log("value", value);
+// setTimeout(function () {
+//   alert("Sau 1s thong bao!");
+// }, 1000);
 
-alert("Hello Chris lee");
-console.log(fullName);
+// setInterval(function () {
+//   console.log(Math.random()); // Thực thi liên tục sau một khoảng thời gian
+// }, 1000);
