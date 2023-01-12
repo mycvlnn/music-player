eb4712e6b8fc32d5c049100d49f641c6d66115e5
Lê Ngoại Ngữ <lengoaingu@gmail.com>
2023-01-12 17:17:42 +0700

    Học về câu lệnh điều kiện if else

--- main.js
+++ main.js
@@ -1,12 +1,20 @@
 /**
- * Kiểu dữ liệu Booleancó 2 value là : true, false
+ * Câu lệnh điều kiện if else
+ * 6 giá trị sau khi convert sang kiểu boolean sẽ là false:
+ * 1. false
+ * 2. 0
+ * 3. '' || ""
+ * 4. null
+ * 5. NaN
+ * 6. undefined
  */
 
 const a = 1;
-const b = 1;
+const b = 2;
+const isSuccess = a > b; // false
 
-console.log(a == b); // true
-
-const c = "Chris";
-const d = "chris";
-console.log(c == d); // false
+if (isSuccess) {
+  console.log("DIEU KIEN DUNG");
+} else {
+  console.log("DIEU KIEN SAI");
+}
