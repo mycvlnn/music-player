c711d9d1daeaabae83dcbbd16a8aff479caf9049
Lê Ngoại Ngữ <lengoaingu@gmail.com>
2023-01-13 14:45:06 +0700

    Học về kiểu dữ liệu trong javascript

--- main.js
+++ main.js
@@ -1,14 +1,46 @@
 /**
-    Học về toán tử logical
+ * Học về kiểu dữ liệu:
  */
 
-const a = 1;
-const b = 2;
-const c = 3;
+// 1. string
+var myName = "Chris Lee";
 
-if (a > b && b > c) {
-  // this is logical operators
-  console.log("DIEU KIEN DUNG");
-} else {
-  console.log("DIEU KIEN SAI");
-}
+console.log(typeof myName); // string
+
+// 2. number
+var testScore = 10;
+
+console.log(typeof testScore);
+
+// 3. undefined
+var a; // Khi định nghĩa một biến mà không gán giá trị -> sẽ là undefined ( chưa định nghĩa)
+console.log(typeof a); // undefined
+
+// 4. null
+var bob = null;
+
+console.log(typeof bob); // object
+
+// 5. Symbol
+var id1 = Symbol("id"); // unique
+var id2 = Symbol("id"); // unique
+const isEqual = id1 === id2;
+console.log(isEqual); // return false
+console.log(typeof id1);
+
+// 6. object
+var student = {
+  id: 1,
+  name: "Le Ngoai Ngu",
+  phone: 012,
+  eat: function () {
+    // todo
+  },
+  sizes: ["1", "2", "3"],
+};
+
+console.log(typeof student);
+
+// 7. array
+var subjects = ["Math", "Literature", "Chemistry"]; // Toan, Van, Hoa
+console.log(typeof subjects);
