c0ba75e7294d6700d569d31ec212ad07e9be9ab1
Lê Ngoại Ngữ <lengoaingu@gmail.com>
2023-01-16 11:35:23 +0700

    Giới thiệu về kiểu dữ liệu số

--- main.js
+++ main.js
@@ -1,59 +1,53 @@
-// Làm việc với chuỗi
-var myString = "Học JS tại F8 JS";
+// Làm việc với số
 
-/**
- * 1. Length
- * 2. Find index
- * 3. Cut string
- * 4. Replace
- * 5. Convert to upper case
- * 6. Convert to lower case
- * 7. Trim
- * 8. Split
- * 9. Get a character by index
- */
+// 1 triệu
+var million = 1000000; //
+// Ngoài cách bên trên ta có thể khai báo bằng chữ e
+var million2 = 1e6; // tương tự 1000000
+var billion = 1e9; // tương tự 1 tỉ (1000000000) // viết kiểu này sẽ không bị nhầm lẫn thiếu số
 
-// 1. Length
-console.log(myString.length); // return 13
+// Đối tượng number. Phương thức sau thường được sử dụng
 
-// 2. Find index
-const index1 = myString.indexOf("JS");
-console.log(index1); // return 4 bắt đầu từ vị trí 0
+// 1. Kiểm tra số hữu hạn là số có giá trị khác Infinity, NaN
+console.log(Number.isFinite(2 / 0)); // false vì value là Infinity
+console.log(Number.isFinite(0 / 0)); // false vì value là NaN
+console.log(Number.isFinite(20 / 5)); // true
+console.log(Number.isFinite(100000000123802138)); // true
+console.log(Number.isFinite(-1)); // return true
 
-const index2 = myString.search("F8"); // cái này có thể search theo regex
-console.log(index2);
+// 2. Number.isInteger() xác định giá trị đã cho có phải là số nguyên hay không. Trả về boolean
+console.log(Number.isInteger(9999999999)); // return true
+console.log(Number.isInteger(-1)); // return true
+console.log(Number.isInteger(10.2)); // false
+console.log(Number.isInteger(Math.PI)); // false
 
-const index3 = myString.lastIndexOf("JS"); // Index sẽ là giá trị tìm thấy cuối cùng
-console.log(index3); // return 14
+// 3. Number.parseFloat() : Chuyển đổi chuỗi đã cho thành một số dấu phẩy động. Tham số là string. Phương thức này khá hay phục vụ cho việc convert giá trị string thành number
+console.log(Number.parseFloat("10")); // 10
+console.log(Number.parseFloat("10.00")); // 10
+console.log(Number.parseFloat("238,21")); // 238
+console.log(Number.parseFloat("237.22")); // 237.22
+console.log(Number.parseFloat("34 56 78")); // 34
+console.log(Number.parseFloat(" 37 ")); // 37
+console.log(Number.parseFloat("18 is my age")); // return 18
 
-// 3. Cut string
-const myString1 = "Xin chao";
-const newString = myString1.slice(4); // return chao
-const newString1 = myString1.substring(0, 3); // return xin
+// 4. Number.parseInt(): Chuyển đổi một chuỗi đã cho thành một số nguyên. Trong trường hợp cố tình truyền số vào nó sẽ tự động convert sang string giúp ta
+Number.parseInt("10"); // 10
+Number.parseInt("10.00"); // 10
+Number.parseInt("238,21"); // 238
+Number.parseInt("237.22"); // 237
+Number.parseInt("34 56 78"); // 34
+Number.parseInt(" 37 "); // 37
+Number.parseInt("18 is my age"); // 18
 
-// 4. Replace
-const myString2 = "Hello Capichi";
-const newString2 = myString2.replace("Hello", "Xin chao"); // return Xin chao Capichi
-const newString3 = myString2.replace(/i/g, "a"); // return Hello Capacha
+// 5. toFixed() Chuyển đổi và trả về chuỗi đại diện cho số đã cho, có số chữ số chính xác sau dấu thập phân. Tham số chỉ nhận giá trị dương
+var myNumber = 1234.56789;
+console.log(myNumber.toFixed()); // '1235' được làm tròn = Math.round(myNumber)
+console.log(myNumber.toFixed(2)); // '1234.56'
+console.log(myNumber.toFixed(NaN)); // '1235' : Đồng nghĩa với việc không truyền vì nó là falsy
+console.log(myNumber.toFixed(6)); // '1234.567890' : Bổ sung thêm số 0 cho đủ 6
 
-// 5. Convert to upper case, lower case
-const myString3 = "Hello guys";
-console.log(myString3.toUpperCase()); // return HELLO GUYS
-console.log(myString3.toLowerCase()); // return hello guys
-
-// 6. trim
-const myString4 = "     hello guys     ";
-console.log(myString4.trim()); // return hello guys
-
-// 7. Split
-
-const myText = "Javascript,PHP,Ruby";
-const myArr = myText.split(",");
-console.log({ myArr });
-
-// 8. Get a character by index
-const myText1 = "Hello world Capichi";
-console.log(myText1[0]); // return H
-console.log(myText1.charAt(0)); // return 'H'
-console.log(myText1.charAt(100)); // return ''
-console.log(myText1[100]); // return undefined
+// 6. toString() Chuyển đổi và trả về số đã cho dưới dạng chuỗi
+console.log((1).toString()); // '1'
+console.log((-1).toString()); // '-1'
+console.log((0).toString()); // '0'
+console.log(NaN.toString()); // 'NaN'
