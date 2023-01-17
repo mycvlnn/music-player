46ca9953d39f458453e1cb00131e16d0b23fd753
Lê Ngoại Ngữ <lengoaingu@gmail.com>
2023-01-17 14:27:08 +0700

    Object in Javascript

--- main.js
+++ main.js
@@ -1,63 +1,42 @@
-// Sắp xếp mảng trong javascript
+// Làm việc với object trong javascript
 
-const fruits = ["Banana", "Orange", "Apple", "Mango"];
+// 1. Tạo object
 
-fruits.sort(); // ['Apple', 'Banana', 'Mango', 'Orange']
-console.log(fruits); // ascending order: ['Apple', 'Banana', 'Mango', 'Orange']
+const newKey = "birth-day";
 
-const numbers = [1, 20, 4, 3, 21];
-console.log(numbers.sort()); // [1, 20, 21, 3, 4]
-
-// 1. Sắp xếp theo thứ tự tăng dần
-const arrAscending = numbers.sort((a, b) => {
-  return a - b;
-});
-
-console.log(arrAscending); // [1, 3, 4, 20, 21]
-
-// 2. Sắp xếp theo thứ tự giảm đần
-const arrDescending = numbers.sort((a, b) => b - a);
-console.log(arrDescending);
-
-// 3. Sorting array of objects
-
-const students = [
-  {
-    id: 1,
-    name: "Hoang",
-    score: 10,
+const student = {
+  id: 1,
+  name: "Chris",
+  age: 18,
+  "my-key": "this is my key",
+  [newKey]: "20/10/2022", // Tạo key dựa vào một biến bên ngoài
+  getName: function () {
+    // Nếu trong trường hợp là function => ta sẽ gọi là method
+    return this.name;
   },
-  {
-    id: 2,
-    name: "Khiem",
-    score: 9,
-  },
-  {
-    id: 3,
-    name: "Khanh",
-    score: 4,
-  },
-  {
-    id: 4,
-    name: "Thuyen",
-    score: -1,
-  },
-  {
-    id: 5,
-    name: "Mai",
-    score: 0,
-  },
-];
+};
 
-// 3.1 Sắp xếp theo id
-const studentsSorted = students.sort((itemA, itemB) => {
-  return itemA.id - itemB.id;
-});
+// 2. Truy xuất value object
 
-console.log(studentsSorted);
+const idStudent = student.id; // 1
+const keyStudent = "name";
 
-// 3.2 Sắp xếp score
-const studentsSorted1 = students.sort((itemA, itemB) => {
-  return itemA.score - itemB.score;
-});
-console.log(studentsSorted1);
+// 2.1 Truy xuất name student dựa vào một biến bên ngoài
+const nameStudent = student[keyStudent];
+console.log(nameStudent); // Chris
+const myBirthDay = student[newKey];
+console.log(myBirthDay);
+
+// 2. 3 Trong trường hợp truy cập vào một key mà không tồn tại trong object đó => return undefined
+const hobby = student.hobby;
+console.log(hobby); // undefined
+
+// 3. Tạo thêm một thuộc tính cho object
+student.phone = 09912321312;
+
+console.log(student);
+
+// 4. Xoá thuộc tính trong object
+delete student.phone;
+
+console.log(student);
