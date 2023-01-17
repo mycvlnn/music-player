703afbe42530012145df4104ce1db2a98a244052
Lê Ngoại Ngữ <lengoaingu@gmail.com>
2023-01-17 17:42:27 +0700

    Câu lệnh rẽ nhánh switch case

--- main.js
+++ main.js
@@ -1,13 +1,28 @@
-// Lệnh rẽ nhánh if else
-
-const date = 2;
-
-if (date === 2) {
-  console.log("Today is monday");
-} else if (date === 3) {
-  console.log("Today is tuesday");
-} else if (date === 4) {
-  console.log("Today is wednesday");
-} else {
-  console.log("Today after wednesday");
+var day;
+switch (new Date().getDay()) {
+  case 0:
+    day = "Sunday";
+    break;
+  case 1:
+    day = "Monday";
+    break;
+  case 2:
+    day = "Tuesday";
+    break;
+  case 3:
+    day = "Wednesday";
+    break;
+  case 4:
+    day = "Thursday";
+    break;
+  case 5:
+    day = "Friday";
+    break;
+  case 6:
+    day = "Saturday";
+    break;
+  default:
+    day = "Unknown Day";
 }
+
+console.log(day); // Tuesday
