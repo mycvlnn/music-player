3431971677cd10d643716d6ad454f5517889efc1
Lê Ngoại Ngữ <lengoaingu@gmail.com>
2023-01-17 17:31:51 +0700

    Câu lệnh rẽ nhánh if else

--- main.js
+++ main.js
@@ -1,41 +1,13 @@
-// Math object
+// Lệnh rẽ nhánh if else
 
-// Math.PI
-const PI = Math.PI;
-console.log(PI); // 3.141592653589793
-// Math.round()
-console.log(Math.round(PI)); // Làm tròn phần nguyên
+const date = 2;
 
-// Math.abs()
-const x = -10;
-console.log(Math.abs(x)); // 10
-// Math.ceil() : Làm tròn trên
-console.log(Math.ceil(PI)); // 4
-
-// Math.floor() : Làm tròn dưới
-console.log(Math.floor(PI)); // 3
-
-// Math.random()
-console.log(Math.random()); // 0.14397898718014113
-
-const gifts = ["Hoa", "Laptop", "Motobike"];
-
-const random = Math.floor(Math.random() * 3);
-
-const myGift = gifts[random];
-console.log(myGift);
-
-// ứng dụng cho việc tỉ lệ radom 1 -> 100%
-const newRandom = Math.floor(Math.random() * 100);
-if (newRandom < 10) {
-  console.log("Ban nhan duoc 10 kim cuong");
+if (date === 2) {
+  console.log("Today is monday");
+} else if (date === 3) {
+  console.log("Today is tuesday");
+} else if (date === 4) {
+  console.log("Today is wednesday");
+} else {
+  console.log("Today after wednesday");
 }
-
-// Math.min()
-const numbers = [1, 2, 3, 4, 5];
-const min = Math.min(...numbers);
-console.log(min); // 1
-
-// Math.max()
-const max = Math.max(...numbers);
-console.log(max); // 5
