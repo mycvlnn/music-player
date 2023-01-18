107c0d82c6f0e36e254bd921c37bfa1e339780fb
Lê Ngoại Ngữ <lengoaingu@gmail.com>
2023-01-18 12:19:23 +0700

    Làm việc với do while loop

--- main.js
+++ main.js
@@ -1,8 +1,21 @@
-// While loop
+// Vòng lặp do while. ứng dụng cho việc nạp thẻ 3 lần trong trường hợp server trả về  lỗi
+
+const isSuccess = false;
+
+const statusServer = true;
 
 var i = 0;
 
-while (i < 100) {
+do {
   i++;
-  console.log(i);
-}
+  console.log(`Nap the lan: ${i}`);
+
+  if (statusServer) {
+    isSuccess = true;
+    console.log("Nap the thanh cong");
+  }
+
+  if (!statusServer && i >= 3) {
+    console.log("nap the that bai");
+  }
+} while (!isSuccess && i <= 3);
