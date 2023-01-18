7c52a56e70c4ca396722e041badf1c09afee7f5d
Lê Ngoại Ngữ <lengoaingu@gmail.com>
2023-01-18 15:51:23 +0700

    Học về cấu trúc dữ liệu và giải thuật

--- main.js
+++ main.js
@@ -1,7 +1,28 @@
-// Javascript Sets
+// Học về đệ quy (recursion)
+/**
+ * 1. Xác định điểm dừng
+ * 2. Logic handle => tạo ra điểm dừng
+ */
 
-const courses = new Set(["javascript", "java", "java", "html", "html"]);
-console.log(courses); // return object set
+// 1. Viết hàm tính giai thừa dựa vào vòng lặp
+const factorial = (number) => {
+  let total = 1;
+  for (let i = number; i > 0; i--) {
+    total *= i;
+  }
 
-const coursesUpdated = [...courses];
-console.log(coursesUpdated); // convert to array
+  return total;
+};
+
+const x = factorial(6);
+console.log(x);
+
+// 2. Viết hàm tính giai thừa dựa vào đệ quy
+const factorial1 = (number) => {
+  if (number <= 1) return 1;
+
+  return number * factorial1(number - 1);
+};
+
+const x2 = factorial1(6);
+console.log(x2);
