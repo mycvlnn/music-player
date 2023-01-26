87458756afd76e73e1f60f6a294c9b69c9ffb70a
Lê Ngoại Ngữ <lengoaingu@gmail.com>
2023-01-26 07:36:27 +0700

    hiểu về phương thức reduce

--- main.js
+++ main.js
@@ -1,89 +1,39 @@
-// Phương thức thao tác với mảng
+// Logic của phương thức reduce
 
-// forEach()	Calls a function for each array element
+Array.prototype.reduce2 = function (callback, initValue) {
+  let result = initValue;
+  let i = 0;
+
+  if (initValue) {
+    i = 1;
+    result = this[0];
+  }
+
+  for (; i < this.length; i++) {
+    result = callback(result, this[i], i, this);
+  }
+
+  return result;
+};
 
 const courses = [
   {
     id: 1,
-    name: "Js",
-    coin: 0,
+    name: "Java",
+    cost: 10,
   },
   {
     id: 2,
-    name: "PHP",
-    coin: 10,
-  },
-  {
-    id: 3,
-    name: "Java",
-    coin: 0,
-  },
-  {
-    id: 4,
-    name: "Ruby",
-    coin: 0,
-  },
-  {
-    id: 5,
-    name: "Dart",
-    coin: 0,
+    name: "Javascript",
+    cost: 2,
   },
 ];
 
-// 1.  forEach()	Calls a function for each array element
-courses.forEach(function (item, index, arr) {
-  console.log(item);
-  console.log(index);
-});
+const handleTotalCoin = (total, item, index, arr) => {
+  console.log(total, item, index, arr);
+  return total + item.cost;
+};
 
-// 2. every() Checks if every element in an array pass a test
-const isAllFree = courses.every(function (item, index, arr) {
-  return item.coin === 0;
-});
+const result = courses.reduce2(handleTotalCoin, 0);
 
-console.log(isAllFree); // false
-
-// 3. some() Checks if any of the elements in an array pass a test
-const hasFree = courses.some(function (item, index, arr) {
-  return item.coin === 0;
-});
-
-console.log(hasFree); // return true
-
-// 4. find() Returns the value of the first element in an array that pass a test
-
-const course = courses.find((item, index, arr) => {
-  return item.coin !== 0;
-});
-
-console.log(course);
-
-// 5. findIndex() 	Returns the index of the first element in an array that pass a test
-const index = courses.findIndex(function (item, index, arr) {
-  return item.name === "Dart";
-});
-
-console.log(index);
-
-// 6. filter
-const listCourses = courses.filter(function (item, index, arr) {
-  return item.coin === 0;
-});
-console.log(listCourses);
-
-// 7. map
-const listCourseUpdated = courses.map((item) => {
-  return {
-    ...item,
-    description: `${item.name}_${item.coin}`,
-  };
-});
-
-console.log(listCourseUpdated);
-
-// 8. reduce: tính tổng tiền của tổng khoá học
-const totalMoney = [].reduce((total, item) => {
-  return total + item.coin;
-}, 0);
-
-console.log({ totalMoney }); // 10
+console.log({ result });
