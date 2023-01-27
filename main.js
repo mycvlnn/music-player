11f33d21b9b129b47a0b1260edd66e464628825b
Lê Ngoại Ngữ <lengoaingu@gmail.com>
2023-01-27 08:29:53 +0700

    Object.hasOwn()

--- main.js
+++ main.js
@@ -1,20 +1,36 @@
-// Empty elements of array
+// ứng dụng for...in, hasOwn để viết lại hàm forEarch2 có cơ chế giống forEarch
 
-var arr = new Array(10); // sẽ tạo ra một mảng toàn phần tử rỗng có độ dài là 10
+const courses = [
+  {
+    id: 1,
+    name: "Java",
+    price: 100,
+  },
+  {
+    id: 2,
+    name: "Javascript",
+    price: 0,
+  },
+  {
+    id: 3,
+    name: "PHP",
+    price: 0,
+  },
+  {
+    id: 4,
+    name: "Ruby",
+    price: 0,
+  },
+];
 
-// Trong trường hợp dùng vòng lặp for để lặp qua:
-const arrLength = arr.length;
+Array.prototype.forEach2 = function (callback) {
+  for (let key in this) {
+    if (Object.hasOwn(this, key)) {
+      callback(this[key], key, this);
+    }
+  }
+};
 
-for (var i = 0; i < arrLength; ++i) {
-  console.log(arr[i]);
-}
-
-// vấn đề là nó sẽ bị lặp qua một mảng mà không có phần từ nào. Vì bản chất i nó cứ chạy khi vẫn thoả mãn điều kiện
-
-// Hướng giải quết
-
-arr.push(1, 2);
-
-for (var index in arr) {
-  console.log(index);
-}
+courses.forEach2(function (item, index, arr) {
+  console.log(item);
+});
