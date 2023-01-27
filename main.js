59e4a6099f1faa1389dabaa55dd3548189817856
Lê Ngoại Ngữ <lengoaingu@gmail.com>
2023-01-27 08:46:37 +0700

    Đối tượng document trong DOM

--- main.js
+++ main.js
@@ -1,36 +1,5 @@
-// ứng dụng for...in, hasOwn để viết lại hàm forEarch2 có cơ chế giống forEarch
+// document object
+console.log(document);
 
-const courses = [
-  {
-    id: 1,
-    name: "Java",
-    price: 100,
-  },
-  {
-    id: 2,
-    name: "Javascript",
-    price: 0,
-  },
-  {
-    id: 3,
-    name: "PHP",
-    price: 0,
-  },
-  {
-    id: 4,
-    name: "Ruby",
-    price: 0,
-  },
-];
-
-Array.prototype.forEach2 = function (callback) {
-  for (let key in this) {
-    if (Object.hasOwn(this, key)) {
-      callback(this[key], key, this);
-    }
-  }
-};
-
-courses.forEach2(function (item, index, arr) {
-  console.log(item);
-});
+// 1. Tìm hiểu phương thức đầu tiên của document.
+document.write("Hello world"); // chú ý: thực tế sẽ không ai làm như thế này cả.
