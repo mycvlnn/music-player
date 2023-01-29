91474c115f2cc4cd28dcc6af7606f9fe86d0bef0
Lê Ngoại Ngữ <lengoaingu@gmail.com>
2023-01-29 11:32:15 +0700

    Promise concept

--- main.js
+++ main.js
@@ -1,12 +1,35 @@
-setTimeout(function () {
-  console.log("work 1");
-  setTimeout(function () {
-    console.log("work 2");
-    setTimeout(function () {
-      console.log("work 3");
-      setTimeout(function () {
-        console.log("work 4");
-      }, 1000);
-    }, 1000);
-  }, 1000);
-}, 1000);
+const isSuccess = true; // biến fake trạng thái gọi api thành công hoặc thất bại
+
+function executor(resolve, reject) {
+  // trong trường hợp thành công call resolve()
+  // trong trường hợp thất bại call reject()
+
+  // FAKE API
+  if (isSuccess) {
+    resolve([
+      {
+        id: 1,
+        name: "Javascript",
+        coin: 2000,
+      },
+    ]);
+  }
+
+  reject("Something wrong. Please try again");
+}
+
+const promise = new Promise(executor);
+
+promise
+  .then(function (data) {
+    console.log({ data });
+    // todo
+  })
+  .catch(function (error) {
+    console.log(error);
+    // todo
+  })
+  .finally(function () {
+    // todo
+    console.log("finally");
+  });
