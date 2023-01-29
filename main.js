7a2068782aeabf36120ff032201da23e3dc83ea0
Lê Ngoại Ngữ <lengoaingu@gmail.com>
2023-01-29 11:22:38 +0700

    Ví dụ về callback hell

--- main.js
+++ main.js
@@ -1,15 +1,12 @@
-const data = '["javascript","PHP", "ruby"]'; // json
-
-console.log(JSON.parse(data)); // return array
-
-const data1 = '{"name": "chris","age": "18", "birthday": "02/03/2003"}';
-console.log(JSON.parse(data1));
-
-const info = {
-  name: "Lee Chris",
-  age: 18,
-  address: "Hong son, My duc, Ha noi",
-};
-
-console.log(JSON.stringify(info));
-console.log(JSON.parse('"string"'));
+setTimeout(function () {
+  console.log("work 1");
+  setTimeout(function () {
+    console.log("work 2");
+    setTimeout(function () {
+      console.log("work 3");
+      setTimeout(function () {
+        console.log("work 4");
+      }, 1000);
+    }, 1000);
+  }, 1000);
+}, 1000);
