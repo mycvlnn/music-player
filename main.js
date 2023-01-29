3480a945868381e545e4865021ce9fd2b1b4603d
Lê Ngoại Ngữ <lengoaingu@gmail.com>
2023-01-29 19:59:05 +0700

    Học async await

--- main.js
+++ main.js
@@ -1,27 +1,16 @@
-// fetch api
-
-const productApi = "https://dummyjson.com/products/search?q=phone";
-const productElement = document.getElementById("product-list");
-
-fetch(productApi)
-  .then(function (reponse) {
-    if (reponse.ok) return reponse.json();
-  })
-  .then(function (data) {
-    console.log(data);
-
-    const products = data.products.map((product) => {
-      return `<li>
-        <h2>${product.title}</h2>
-        <p>${product.description}</p>
-        <img src=${product.thumbnail} />
-      </li>`;
-    });
-
-    const html = products.join();
-
-    productElement.innerHTML = html;
-  })
-  .catch(function (reason) {
-    productElement.innerHTML = `<h1>${reason.message}</h1>`;
+function resolveAfter2Seconds() {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve("resolved");
+    }, 2000);
   });
+}
+
+async function asyncCall() {
+  console.log("calling");
+  const result = await resolveAfter2Seconds();
+  console.log(result);
+  // Expected output: "resolved"
+}
+
+asyncCall();
