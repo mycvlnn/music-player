6d2dfe2657916d1462d1cb12e6c903fa0e4628a4
Lê Ngoại Ngữ <lengoaingu@gmail.com>
2023-01-29 14:53:13 +0700

    Promise static method

--- main.js
+++ main.js
@@ -1,24 +1,37 @@
-// Viết hàm sleep
-const sleep = function (ms) {
-  return new Promise((resolve) => {
-    setTimeout(resolve, ms);
-  });
-};
+// Promise static method
 
-sleep(1000)
-  .then(function () {
-    console.log(1);
-    return sleep(1000);
+// 1. Promise.resolve
+const promise = Promise.resolve([1, 2, 3]);
+
+promise.then(function (data) {
+  console.log("data", data); // [1, 2, 3]
+});
+
+// 2. Promise.reject
+const promise2 = Promise.reject("Something went wrong!");
+promise2.catch((error) => console.log(error)); // Something went wrong!
+
+// 3. Promise.all
+
+const newPromise1 = new Promise((resolve) => {
+  setTimeout(function () {
+    resolve([1, 2, 3]);
+  }, 1000);
+});
+
+const newPromise2 = new Promise((resolve) => {
+  setTimeout(function () {
+    resolve([4, 5, 6]);
+  }, 2000);
+});
+
+const promiseAll = Promise.all([newPromise1, newPromise2]);
+
+promiseAll
+  .then(function ([data1, data2]) {
+    const numbers = data1.concat(data2);
+    console.log({ numbers });
   })
-  .then(function () {
-    console.log(2);
-    return sleep(1000);
-  })
-  .then(function () {
-    console.log(3);
-    return sleep(1000);
-  })
-  .then(function () {
-    console.log(4);
-    return sleep(1000);
+  .catch((error) => {
+    console.log(error);
   });
