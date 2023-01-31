d7765ad4c55068f228080248b5420c9168ede772
Lê Ngoại Ngữ <lengoaingu@gmail.com>
2023-01-31 21:15:11 +0700

    arrow function

--- main.js
+++ main.js
@@ -1,12 +1,26 @@
-// Template string
+// Arrow function
 
-const fullName = "Le Van Ngoc";
-const age = 18;
+const sum = (a, b) => a + b;
 
-const info = `${fullName} ${age}`;
+console.log(1, 2); // 3
 
-// Multiple line
-const lines = `
-Line 1
-Line 2
-`;
+const logger = (data) => data;
+
+const renderCourse = () => ({ id: 1, name: "Javascript" });
+
+const info = {
+  name: "Chris",
+  getName: () => {
+    return this; // con trỏ this này sẽ là đối tượng window.
+  },
+};
+
+console.log(info.getName());
+
+// Không thể định nghĩa Object constructor bằng arrow function
+const Car = (name, color) => {
+  this.name = name;
+  this.color = color;
+};
+
+const car = new Car("Mercerdies", "#fff"); // error
