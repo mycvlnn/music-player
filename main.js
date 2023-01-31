c5b774abc4f69011243b773772f5d5be3ee03ec4
Lê Ngoại Ngữ <lengoaingu@gmail.com>
2023-01-31 20:51:31 +0700

    template literals and multiple lines

--- main.js
+++ main.js
@@ -1,23 +1,12 @@
-const PI = 3.141592653589793;
-PI = 3.14; // This will give an error
-PI = PI + 10; // This will also give an error
+// Template string
 
-// You can create a const object:
-const car = { type: "Fiat", model: "500", color: "white" };
+const fullName = "Le Van Ngoc";
+const age = 18;
 
-// You can change a property:
-car.color = "red";
+const info = `${fullName} ${age}`;
 
-// You can add a property:
-car.owner = "Johnson";
-
-let x = "John Doe";
-
-let x = 0;
-
-// SyntaxError: 'x' has already been declared
-
-{
-  let x = 2;
-}
-// x can NOT be used here
+// Multiple line
+const lines = `
+Line 1
+Line 2
+`;
