1ad0bedca2e3c3870114cd5ed45de6a2177543b8
Lê Ngoại Ngữ <lengoaingu@gmail.com>
2023-01-31 21:32:46 +0700

    học về class basic

--- main.js
+++ main.js
@@ -1,26 +1,13 @@
-// Arrow function
+// classes basic
 
-const sum = (a, b) => a + b;
+class Course {
+  constructor(id, name) {
+    this.id = id;
+    this.name = name;
+  }
+}
 
-console.log(1, 2); // 3
+const javascript = new Course(1, "Javascript");
+const php = new Course(2, "PHP");
 
-const logger = (data) => data;
-
-const renderCourse = () => ({ id: 1, name: "Javascript" });
-
-const info = {
-  name: "Chris",
-  getName: () => {
-    return this; // con trỏ this này sẽ là đối tượng window.
-  },
-};
-
-console.log(info.getName());
-
-// Không thể định nghĩa Object constructor bằng arrow function
-const Car = (name, color) => {
-  this.name = name;
-  this.color = color;
-};
-
-const car = new Car("Mercerdies", "#fff"); // error
+console.log({ javascript, php });
