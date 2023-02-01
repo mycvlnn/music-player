7037ab05a05408f91f95244e129322f8a7f8d0ca
Lê Ngoại Ngữ <lengoaingu@gmail.com>
2023-02-01 21:58:41 +0700

    tagged template literals

--- main.js
+++ main.js
@@ -1,29 +1,18 @@
-// spread
+// Viết function dùng để highlight các biến cần thiết bằng tagged template string
 
-// 1. array
-const arr1 = [1, 2, 3];
-const arr2 = [1, 2, 3];
-const arr3 = [...arr1, ...arr2];
+const highlight = ([firstValue, ...strings], ...values) => {
+  console.log(strings, values);
 
-console.log(arr3); // [1, 2, 3, 1, 2, 3]
-
-// 2. object
-
-const course = {
-  id: 1,
-  name: "Java",
-  coin: 1000,
+  const htmls = values
+    .reduce(
+      (acc, curr) => [...acc, `<span>${curr}</span>`, strings.shift()],
+      [firstValue]
+    )
+    .join("");
+  console.log(htmls);
 };
 
-const course2 = { ...course, id: 2 };
-console.log(course2); // {id: 2, name: 'Java', coin: 1000}
+const brand = "F8";
+const course = "Javascript";
 
-// 3. function
-
-const data = [1, 2, 3, 4];
-
-const logger = (a, b, c, d) => {
-  console.log(a, b, c, d);
-};
-
-logger(...data);
+highlight`Học lập trình ${course} tại ${brand}`;
