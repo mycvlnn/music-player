326f7ae087c498b0dae5c341696a9b327c4463cf
Lê Ngoại Ngữ <lengoaingu@gmail.com>
2023-02-02 21:49:07 +0700

    nullish operator

--- main.js
+++ main.js
@@ -1,38 +1,9 @@
-// Modules in javascript
-import { TYPE_ERROR, TYPE_LOG, TYPE_WARN as WARN } from "./constants.js";
+// nullish operator
 
-import logger from "./logger.js";
+const foo = null ?? "default string";
+console.log(foo);
+// Expected output: "default string"
 
-logger("hello", TYPE_ERROR);
-
-// Optional chaining
-
-const info = {
-  name1: "chris",
-  children: {
-    nam2: "Chris child",
-  },
-};
-
-console.log(info?.children1?.name); // sẽ không bị lỗi và return undefined
-
-console.log(info?.["test"]?.name); // undefined. Không bị lỗi
-
-// Câu lệnh trên tương đương
-// if (info.test && info.test.name) {
-//   console.log(info.test.name);
-// }
-
-const arr1 = [{ name: "chris", age: 1, id: 0 }];
-
-const itemArr = arr1.find((item) => item.id === 10)?.age;
-
-// Câu lệnh trên tương đương với câu lệnh:
-if (itemArr) console.log(itemArr.age);
-
-const objectInfo = {
-  // getName(){
-  // }
-};
-
-console.log(objectInfo.getName?.());
+const baz = 0 ?? 42;
+console.log(baz);
+// Expected output: 0
