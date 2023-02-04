cd7e464aa05eeab9c89a3515220d1910e6edf610
Lê Ngoại Ngữ <lengoaingu@gmail.com>
2023-02-04 16:50:32 +0700

    Xây dựng logic tab UI căn bản

--- logger.js
+++
@@ -1,7 +0,0 @@
-import { TYPE_LOG } from "./constants.js";
-
-const logger = (message, type = TYPE_LOG) => {
-  console[type](message);
-};
-
-export default logger;
--- main.js
+++ main.js
@@ -1,9 +1,29 @@
-// nullish operator
+const $ = document.querySelector.bind(document);
+const $$ = document.querySelectorAll.bind(document);
 
-const foo = null ?? "default string";
-console.log(foo);
-// Expected output: "default string"
+const tabItems = $$(".tab-item");
+const tabPanes = $$(".tab-pane");
+const tabActive = $(".tab-item.active");
+const line = $(".tabs .line");
 
-const baz = 0 ?? 42;
-console.log(baz);
-// Expected output: 0
+function setLineStyle(left, width) {
+  line.style.left = left + "px";
+  line.style.width = width + "px";
+}
+
+setLineStyle(tabActive.offsetLeft, tabActive.offsetWidth);
+
+tabItems.forEach((tabItem, index) => {
+  const pane = tabPanes[index];
+
+  tabItem.onclick = function (e) {
+    const tabPaneActive = $(".tab-pane.active");
+    const tabActiveUpdate = $(".tab-item.active");
+    tabActiveUpdate.classList.remove("active");
+    tabPaneActive.classList.remove("active");
+
+    this.classList.add("active");
+    pane.classList.add("active");
+    setLineStyle(this.offsetLeft, this.offsetWidth);
+  };
+});
